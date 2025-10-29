5df187a2ac55830380cb20e753150a410de20891
PedroAugustokkkk <contatodepedro@gmail.com>
2025-10-29 16:39:48 -0300

    modificação views

--- views.py
+++ views.py
@@ -3,12 +3,11 @@ from main import *
 # rotas
 @app.route ("/")
 def homepage():
-    # return "<h1>Meu Site Flask</h1>"
     nome = "Pizzaria duGordo"
     sabores = ["Pepperoni", "4 Queijos", "Marguerita"]
-    return render_template ("index.html", nome=nome, sabores=sabores)
+    return render_template ("home.html", nome=nome, sabores=sabores)
 
-# localhost:5000/user/daniel
+
 @app.route ("/user/<name>")
 def user(name):
-    return render_template ("user.html", name=name)
+    return render_template ("cardapio.html", name=name)
